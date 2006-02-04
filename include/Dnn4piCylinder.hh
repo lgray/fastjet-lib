d9c859bd0f347cbd95e1692854d1954c81af250d
Gavin Salam <gavin.salam@cern.ch>
2006-02-04 18:43:44 +0000

    Added minimal GPL files and headers
    git-svn-id: file:///Users/gsalam/svn/fastjet/trunk/fastjet-release@95 9ab96302-e443-4389-8a14-f726967065a6

--- include/Dnn4piCylinder.hh
+++ include/Dnn4piCylinder.hh
@@ -1,3 +1,34 @@
+//STARTHEADER
+// $Id$
+//
+// Copyright (c) 2005-2006, Matteo Cacciari and Gavin Salam
+//
+//----------------------------------------------------------------------
+// This file is part of FastJet.
+//
+//  FastJet is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  The algorithms that underlie FastJet have required considerable
+//  development and are described in hep-ph/0512210. If you use
+//  FastJet as part of work towards a scientific publication, please
+//  include a citation to the FastJet paper.
+//
+//  FastJet is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with FastJet; if not, write to the Free Software
+//  Foundation, Inc.:
+//      59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//----------------------------------------------------------------------
+//ENDHEADER
+
+
 #ifndef DROP_CGAL // in case we do not have the code for CGAL
 #ifndef __DNN4PICYLINDER__
 #define __DNN4PICYLINDER__
