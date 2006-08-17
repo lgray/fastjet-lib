c135351dcd04b3f602cf18160aa44e9a429ba30b
Gavin Salam <gavin.salam@cern.ch>
2006-08-17 09:32:28 +0000

    added include/fastjet/base.hh and moved all include files into include/fastjet
    git-svn-id: file:///Users/gsalam/svn/fastjet/trunk/fastjet-release@273 9ab96302-e443-4389-8a14-f726967065a6

---
+++ include/fastjet/base.hh
@@ -0,0 +1,10 @@
+
+#ifndef __FASTJET_BASE_HH__
+#define __FASTJET_BASE_HH__
+
+// define this for easier readability (and obfuscation?) in
+// a range of places
+#define FASTJET_BEGIN_NAMESPACE namespace fastjet {
+#define FASTJET_END_NAMESPACE   }
+
+#endif // __FASTJET_BASE_HH__
