29a8198313e97371616fca70429edec4bbc89496
Gavin Salam <gavin.salam@cern.ch>
2006-09-12 13:47:01 +0000

    switched to cam-r0.25 run
    git-svn-id: file:///Users/gsalam/svn/fastjet/trunk/fastjet-release@316 9ab96302-e443-4389-8a14-f726967065a6

--- areas/run-HI-incplt.pl
+++ areas/run-HI-incplt.pl
@@ -3,8 +3,8 @@
 $iseqlo=0;
 $nseq=10;
 
-$r=0.4;
-#$r=0.25;
+#$r=0.4;
+$r=0.25;
 
 $cam=1;
 #$cam=0;
@@ -32,15 +32,15 @@ for ($iseq=$iseqlo; $iseq < $iseqlo+$nseq; $iseq++) {
   ### INTRODUCED A NEW OUTPUT FORMAT
   #$command = "submitjob.pl BEST ./run-mass-test.pl -nev 50000 -out hydjet_inclpt/incplt_xcl_ptminhard50_nhsel2".$camname."_r".$r."_iseq$iseqformat $camopt -r $r -nhsel 2 -ptmin 10 -ptminhard 50 -ptmaxhard -1 -freq 100 -iseq $iseq";
 
-  # $command = "submitjob.pl BEST ./run-mass-test.pl -nev 100000 -out hydjet_inclpt/incplt_xcl_nhsel1".$camname."_r".$r."_iseq$iseqformat $camopt -r $r -nhsel 1 -ptmin 10 -ptminhard 10 -ptmaxhard -1 -freq 100 -iseq $iseq -strategy -4";
-  # 
-  # #print $command."\n";
-  # system("$command");
-  # 
-  # $command = "submitjob.pl BEST ./run-mass-test.pl -nev 100000 -out hydjet_inclpt/incplt_xcl_ptminhard50_nhsel1".$camname."_r".$r."_iseq$iseqformat $camopt -r $r -nhsel 1 -ptmin 10 -ptminhard 50 -ptmaxhard -1 -freq 100 -iseq $iseq  -strategy -4";
-  # 
-  # #print $command."\n";
-  # system("$command");
+  $command = "submitjob.pl BEST ./run-mass-test.pl -nev 100000 -out hydjet_inclpt/incplt_xcl_nhsel1".$camname."_r".$r."_iseq$iseqformat $camopt -r $r -nhsel 1 -ptmin 10 -ptminhard 10 -ptmaxhard -1 -freq 100 -iseq $iseq -strategy -4";
+  
+  #print $command."\n";
+  system("$command");
+  
+  $command = "submitjob.pl BEST ./run-mass-test.pl -nev 100000 -out hydjet_inclpt/incplt_xcl_ptminhard50_nhsel1".$camname."_r".$r."_iseq$iseqformat $camopt -r $r -nhsel 1 -ptmin 10 -ptminhard 50 -ptmaxhard -1 -freq 100 -iseq $iseq  -strategy -4";
+  
+  #print $command."\n";
+  system("$command");
 
 
   for ($nhsel=3; $nhsel<=3; $nhsel++) {
