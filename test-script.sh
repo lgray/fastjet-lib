34c4ecbe9fe08bc33ac13d33131f3044afaf64c4
Matteo Cacciari <matteo.cacciari@cern.ch>
2007-01-23 19:16:11 +0000

    some changes in test-script.sh
    git-svn-id: file:///Users/gsalam/svn/fastjet/trunk/fastjet-release@440 9ab96302-e443-4389-8a14-f726967065a6

--- test-script.sh
+++ test-script.sh
@@ -1,30 +1,40 @@
 #!/bin/bash
 # Script for testing whether everything works!
 
-#CLEAN=""
+
+# set G77LIBDIR if the user has not set it in its environment
+if [[ ! $G77LIBDIR ]] ; then
+    dir=`locate libg2c.a | head -1`
+    G77LIBDIR=`dirname $dir`/
+    echo Setting G77LIBDIR to $G77LIBDIR
+fi
+
+
+# set CLEAN to "empty string" in order NOT to clean
+CLEAN=""
+
 #if [ $# > 0 ];then
 #if [ $1 == "-c" ]; then
 #CLEAN="1"
 #fi
 #fi
 
+
 # build the main package
 pushd src
-#if [ $CLEAN ]; then make clean || exit -1; fi
-make clean || exit -1
-make || exit -1
+if [ $CLEAN ]; then make clean || exit -1; fi
 make install
 popd
 
 # build the plugins
 pushd plugins
-make clean || exit -1
+if [ $CLEAN ]; then make clean || exit -1; fi
 make || exit -1
 popd
 
 # build the examples and test a couple
 pushd example
-make clean || exit -1
+if [ $CLEAN ]; then make clean || exit -1; fi
 make fastjet_example  || exit -1
 ./fastjet_example < data/single-event.dat || exit -1
 make fastjet_areas || exit -1
@@ -37,7 +47,7 @@ popd
 
 # try out some plugin examples
 pushd plugins/usage_examples
-make clean || exit -1
+if [ $CLEAN ]; then make clean || exit -1; fi
 make cdfmidpoint_example many_algs_example pxcone_example siscone_example || exit -1
 ./many_algs_example < data/single-event.dat || exit -1
 popd
