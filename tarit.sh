0b29a4ce5a24e920121a2f421dbc90144d41f90e
Gavin Salam <gavin.salam@cern.ch>
2007-02-08 16:43:38 +0000

    minor mod
    git-svn-id: file:///Users/gsalam/svn/fastjet/trunk/fastjet-release@453 9ab96302-e443-4389-8a14-f726967065a6

--- tarit.sh
+++ tarit.sh
@@ -69,7 +69,7 @@ else
   echo "Putting original Makefile back"
   mv -v Makefile.orig Makefile
 
-  #
+  # reminders about what to do for svn
   URL=`svn info | grep URL | sed 's/^.*URL: //'`
   tagURL=`echo $URL | sed "s/trunk\/fastjet-release/tags\/fastjet-$version/"`
   echo "Remember to tag the version:"
