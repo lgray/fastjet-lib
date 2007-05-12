7f46910f7d56253d18ab862359b016af7d149887
Matteo Cacciari <matteo.cacciari@cern.ch>
2007-05-12 15:54:47 +0000

    updated areas-2point.cc to also calculate siscone
    git-svn-id: file:///Users/gsalam/svn/fastjet/trunk/fastjet-release@638 9ab96302-e443-4389-8a14-f726967065a6

--- areas/areas-2point.cc
+++ areas/areas-2point.cc
@@ -1,7 +1,7 @@
 #include "fastjet/PseudoJet.hh"
 #include "fastjet/ClusterSequence.hh"
 #include "fastjet/ClusterSequenceActiveArea.hh"
-#include "ClusterSequencePassiveArea.hh"
+//#include "ClusterSequencePassiveArea.hh"
 #include<iostream>
 #include<sstream>
 #include<fstream>
@@ -11,6 +11,10 @@
 #include<cstddef> // for size_t
 #include "CmdLine.hh"
 #include<cmath>
+
+// get the plugins
+#include "SISConePlugin.hh"
+#include "CDFMidPointPlugin.hh"
 
 namespace fj = fastjet;
 using namespace std;
@@ -27,22 +31,43 @@ int main (int argc, char ** argv) {
   double ktR   = cmdline.double_val("-r",1.0);
   double ghost_area = cmdline.double_val("-ghost_area",cmdline.double_val("-cell_area",0.01));
   double ghost_etamax = cmdline.double_val("-ghost_etamax",6.0);
-  double grid_scatter = cmdline.double_val("-grid_scatter",0.0001);
+  double grid_scatter = cmdline.double_val("-grid_scatter",1.);
   double kt_scatter   = cmdline.double_val("-kt_scatter",0.1);
   int    n            = cmdline.int_val("-n",20);
   double dr_max       = cmdline.double_val("-dr_max",2.1);
   double dr_min       = cmdline.double_val("-dr_min",0.0);
 
-  fj::JetFinder jet_finder = cmdline.present("-cam") ? 
-                                fj::cambridge_algorithm : fj::kt_algorithm;
+
+  fj::RecombinationScheme  rec_scheme  = cmdline.present("-pt_scheme") ? fj::BIpt_scheme :
+              							       fj::E_scheme;
+
+  // create the definitions for our jet finder...
+  fj::JetDefinition jet_def;
+  if (cmdline.present("-cam")) {
+    jet_def = fj::JetDefinition(fj::cambridge_algorithm, ktR, rec_scheme, strategy);}
+  else if (cmdline.present("-kt")) {
+    jet_def = fj::JetDefinition(fj::kt_algorithm, ktR, rec_scheme, strategy);}
+  else if (cmdline.present("-midpoint")) {
+    double overlap = cmdline.value("-f",0.5);
+    double seed    = cmdline.value("-seed",0.0);
+    jet_def = fj::JetDefinition(new fj::CDFMidPointPlugin(ktR,overlap,seed));}
+  else if (cmdline.present("-siscone")) {
+    double overlap = cmdline.double_val("-f",0.5);
+    int    npass   = cmdline.int_val("-npass",1);
+    fj::SISConePlugin * plugin = new fj::SISConePlugin(ktR,overlap,npass);
+    if (cmdline.present("-smstop"))plugin->set_split_merge_stopping_scale(1e-50);
+    jet_def = fj::JetDefinition(plugin);}
+  else {
+    cerr << "Must specify one of -kt | -cam | -siscone" << endl;
+    exit(-1);
+  }
 
 
   if (!cmdline.all_options_used()) {cerr << 
       "Error: some options unsupported"<<endl; 
     exit(-1);}
 
-  // create the definitions for our jet finder and areas spec...
-  fj::JetDefinition jet_def(jet_finder, ktR, strategy);
+  // create the definitions for our areas spec...
   fj::ActiveAreaSpec active_area_spec(ghost_etamax, repeat, ghost_area, 
                                       grid_scatter, kt_scatter);
 
