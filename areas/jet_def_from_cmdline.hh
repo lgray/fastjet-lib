bf81b81d71f7a21d9c4004bf763c9eb49e6dd7ca
Matteo Cacciari <matteo.cacciari@cern.ch>
2007-06-16 08:26:53 +0000

    small changes in ReleaseNotes, move areas out of fastjet-release
    git-svn-id: file:///Users/gsalam/svn/fastjet/trunk/fastjet-release@732 9ab96302-e443-4389-8a14-f726967065a6

--- areas/jet_def_from_cmdline.hh
+++
@@ -1,9 +0,0 @@
-#include "CmdLine.hh"
-#include "fastjet/JetDefinition.hh"
-#include "fastjet/AreaDefinition.hh"
-
-/// return a jet definition based on the command line...
-fastjet::JetDefinition jet_def_from_cmdline(const CmdLine & cmdline);
-
-/// return an area definition based on the command line...
-fastjet::AreaDefinition area_def_from_cmdline(const CmdLine & cmdline);
--- areas/run-areas.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/zsh
-
-./areas -repeat 6000 -dense -incl 70.0 -strategy 2 < ../data/single-event.dat
