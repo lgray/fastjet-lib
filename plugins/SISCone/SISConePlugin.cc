84680112d86fef52041816808665ce9b6b4b7436
Gavin Salam <gavin.salam@cern.ch>
2007-07-12 13:34:29 +0000

    removed endl in SISCone version output
    git-svn-id: file:///Users/gsalam/svn/fastjet/trunk/fastjet-release@793 9ab96302-e443-4389-8a14-f726967065a6

--- plugins/SISCone/SISConePlugin.cc
+++ plugins/SISCone/SISConePlugin.cc
@@ -37,13 +37,15 @@ string SISConePlugin::description () const {
   desc <<  sm_scale_string                                << ", ";
   desc << "caching turned "      << (caching() ? on : off);
   desc << ", SM stop scale = "     << _split_merge_stopping_scale;
-  desc << ", SISCone code v" << VERSION << endl;
+
 
   // create a fake siscone object so that we can find out more about it
   Csiscone siscone;
   if (siscone.merge_identical_protocones) {
     desc << ", and (IR unsafe) merge_indentical_protocones=true" ;
   }
+
+  desc << ", SISCone code v" << VERSION;
 
   return desc.str();
 }
