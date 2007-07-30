4a9d2d20e8d82b438ea09164e4afe96569493d19
Gavin Salam <gavin.salam@cern.ch>
2007-07-30 11:50:30 +0000

    removed pedantic from src/Makefile for CGAL3.3 and added output of fastjet version to fastjet_timing_plugins
    git-svn-id: file:///Users/gsalam/svn/fastjet/trunk/fastjet-release@837 9ab96302-e443-4389-8a14-f726967065a6

--- example/timings/timings.pl
+++ example/timings/timings.pl
@@ -36,14 +36,15 @@ $baserep = 700;
 #@strategy = (11,102);
 #@strategy = (-3,-1,2);
 #@strategy = (102);
+@strategy = (2);
 # for cones
 #@strategy = (1,204,203,101,201,202,212);
-@strategy = (204);
+#@strategy = (204);
 #@strategy = (11,102);
 
 #$radius=0.4;
-$radius=0.7;
-#$radius=1.0;
+#$radius=0.7;
+$radius=1.0;
 
 # number of runs to average over when getting
 # timings. Remember that first run will be discarded
