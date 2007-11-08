24850ead46b43d4ca44e840f3a29d41698202512
Gavin Salam <gavin.salam@cern.ch>
2007-11-08 14:53:44 +0000

    restored backwards compatibility for plugin locations
    git-svn-id: file:///Users/gsalam/svn/fastjet/trunk/fastjet-release@941 9ab96302-e443-4389-8a14-f726967065a6

---
+++ plugins/PxCone/PxConePlugin.hh
@@ -0,0 +1,5 @@
+// for backwards compatibility fastjet/PxConePlugin.hh can also be
+// accessed through this file without specifying "fastjet/"; this is
+// however deprecated for new code, and is not guaranteed to work from
+// release 3.x onwards
+#include "fastjet/PxConePlugin.hh"
