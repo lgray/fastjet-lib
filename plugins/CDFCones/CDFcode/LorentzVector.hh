d2d8bebea5593ca6cc56254ba4e44151b6274a8c
Gavin Salam <gavin.salam@cern.ch>
2008-01-14 20:23:34 +0000

    added fix for caching y,phi in CDF code
    git-svn-id: file:///Users/gsalam/svn/fastjet/trunk/fastjet-release@1007 9ab96302-e443-4389-8a14-f726967065a6

--- plugins/CDFCones/CDFcode/LorentzVector.hh
+++ plugins/CDFCones/CDFcode/LorentzVector.hh
@@ -8,22 +8,30 @@ class LorentzVector
  public:
 
   double px,py,pz,E;
+  mutable double phi_store, rap_store ;
+  const static double bad_phi = -10.0;
 
-  LorentzVector(): px(0), py(0), pz(0), E(0) {}
-  LorentzVector(double p1, double p2, double p3, double p0): px(p1), py(p2), pz(p3), E(p0) {}
-  LorentzVector(const LorentzVector& p): px(p.px), py(p.py), pz(p.pz), E(p.E) {}
+  LorentzVector(): px(0), py(0), pz(0), E(0), phi_store(bad_phi) {}
+  LorentzVector(double p1, double p2, double p3, double p0): px(p1), py(p2), pz(p3), E(p0), phi_store(bad_phi)  {}
+  LorentzVector(const LorentzVector& p): px(p.px), py(p.py), pz(p.pz), E(p.E), phi_store(bad_phi)  {}
   double p()   const {return sqrt(px*px + py*py + pz*pz);}
   double pt()  const {return sqrt(px*px + py*py);}
   double mt()  const {return sqrt((E-pz)*(E+pz));}
-  double y()   const {return 0.5*log((E + pz)/(E - pz));}
+  //double y()   const {return 0.5*log((E + pz)/(E - pz));}
   double Et()  const {return E/p()*pt();}
   double eta() const {return 0.5*log((p() + pz)/(p() - pz));}
   double phi() const
   {
-    double r = atan2(py,px);
-    if(r < 0)
-      r += 2*M_PI;
-    return r;
+    if (phi_store == bad_phi) calc_phi_rap();
+    return phi_store;
+    // double r = atan2(py,px);
+    // if(r < 0)
+    //   r += 2*M_PI;
+    // return r;
+  }
+  double y()   const {
+    if (phi_store == bad_phi) calc_phi_rap();
+    return rap_store;
   }
   void add(LorentzVector v)
   {
@@ -31,6 +39,7 @@ class LorentzVector
     py += v.py;
     pz += v.pz;
     E  += v.E;
+    phi_store = bad_phi;
   }
   void subtract(LorentzVector v)
   {
@@ -38,10 +47,17 @@ class LorentzVector
     py -= v.py;
     pz -= v.pz;
     E  -= v.E;
+    phi_store = bad_phi;
   }
   bool isEqual(LorentzVector v)
   {
     return px == v.px && py == v.py && pz == v.pz && E == v.E;
+  }
+
+  void calc_phi_rap() const {
+    phi_store = atan2(py,px);
+    if(phi_store < 0) phi_store += 2*M_PI;
+    rap_store = 0.5*log((E + pz)/(E - pz));
   }
 };
 
