4bb43b5a688cc6f2120980717541c2047fbda022
Gregory Soyez <gregory.soyez@cern.ch>
2008-06-03 22:49:25 +0000

    added the FastJet header
    git-svn-id: file:///Users/gsalam/svn/fastjet/trunk/fastjet-release@1230 9ab96302-e443-4389-8a14-f726967065a6

--- plugins/NestedAlgs/NestedAlgsPlugin.cc
+++ plugins/NestedAlgs/NestedAlgsPlugin.cc
@@ -1,3 +1,33 @@
+//STARTHEADER
+// $Id: ClusterSequence_DumbN3.cc 293 2006-08-17 19:38:38Z salam $
+//
+// Copyright (c) 2007-2008, Matteo Cacciari, Gavin Salam and Gregory Soyez
+//
+//----------------------------------------------------------------------
+// This file is part of FastJet.
+//
+//  FastJet is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  The algorithms that underlie FastJet have required considerable
+//  development and are described in hep-ph/0512210. If you use
+//  FastJet as part of work towards a scientific publication, please
+//  include a citation to the FastJet paper.
+//
+//  FastJet is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with FastJet; if not, write to the Free Software
+//  Foundation, Inc.:
+//      59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//----------------------------------------------------------------------
+//ENDHEADER
+
 // fastjet stuff
 #include "fastjet/ClusterSequence.hh"
 #include "fastjet/NestedAlgsPlugin.hh"
