6b7084bdf3501835a87f509b5e0bdbdd7308b6d5
Gavin Salam <gavin.salam@cern.ch>
2008-09-04 14:52:33 +0000

    in JetClu plugin, caused jet constituents to be sorted before transferring back to the cluster sequence
    git-svn-id: file:///Users/gsalam/svn/fastjet/trunk/fastjet-release@1287 9ab96302-e443-4389-8a14-f726967065a6

--- plugins/CDFCones/CDFJetCluPlugin.cc
+++ plugins/CDFCones/CDFJetCluPlugin.cc
@@ -100,34 +100,43 @@ void CDFJetCluPlugin::run_clustering(ClusterSequence & clust_seq) const {
   //  int ntow = int(jetIter->towerList.size());
 
   for(int iCDFjets = jets.size()-1; iCDFjets >= 0; iCDFjets--) {
+
     const vector<PhysicsTower> & tower_list = jets[iCDFjets].towerList;
-    //int jet_k = jetmap[tower_list[0].fourVector.E];
-    int jet_k = tower_list[0].fjindex;
-  
     int ntow = int(tower_list.size());
+    
+    // 2008-09-04: sort the towerList (according to fjindex) so as
+    //             to have a consistent order for particles in jet
+    //             (necessary because addition of ultra-soft particles
+    //             sometimes often modifies the order, while maintaining
+    //             the same overall set)
+    vector<int>    jc_indices(ntow);
+    vector<double> fj_indices(ntow); // use double: benefit from existing routine
+    for (int itow = 0; itow < ntow; itow++) {
+      jc_indices[itow] = itow;
+      fj_indices[itow] = tower_list[itow].fjindex;
+    }
+    sort_indices(jc_indices, fj_indices);
+
+    int jet_k = tower_list[jc_indices[0]].fjindex;
+  
     for (int itow = 1; itow < ntow; itow++) {
+      if (tower_list[jc_indices[itow]].Et() > 1e-50) {
+      }
       int jet_i = jet_k;
-      // retrieve our misappropriated index for the jet
+      // retrieve our index for the jet
       int jet_j;
-      jet_j = tower_list[itow].fjindex;
-      //int alt_jet_j = jetmap[tower_list[itow].fourVector.E];
-      //assert(jet_j == alt_jet_j);
-      //cout << jet_j << endl;
+      jet_j = tower_list[jc_indices[itow]].fjindex;
+
       // safety check
       assert (jet_j >= 0 && jet_j < int(towers.size()));
+
       // do a fake recombination step with dij=0
       double dij = 0.0;
+
       // JetClu does E-scheme recombination so we can stick with the
       // simple option
       clust_seq.plugin_record_ij_recombination(jet_i, jet_j, dij, jet_k);
-      //if (itow != ntow) {
-      //  clust_seq.plugin_record_ij_recombination(jet_i, jet_j, dij, jet_k);
-      //} else {
-      //  clust_seq.plugin_record_ij_recombination(jet_i, jet_j, dij, 
-      //       PseudoJet(jetIter->fourVector.px,jetIter->fourVector.py,
-      //                 jetIter->fourVector.pz,jetIter->fourVector.E),
-      //                                           jet_k);
-      //}
+
     }
   
     // NB: put a sensible looking d_iB just to be nice...
@@ -151,22 +160,6 @@ void CDFJetCluPlugin::run_clustering(ClusterSequence & clust_seq) const {
   //cout << endl;
 }
 
-
-//// following code should now be obsolete since addition of 
-//// index to the physics tower in the CDF code
-// void CDFJetCluPlugin::_insert_unique(PseudoJet & jet, 
-//                                      map<double,int> & jetmap) const {
-//   while (jetmap.find(jet.E()) != jetmap.end()) {
-//     // deal with cases where something else has the same energy, and
-//     // also with situation where that energy is zero.
-//     if (jet.E() != 0.0) {
-//       jet *= 1.0+1e-12;
-//     } else {
-//       jet += PseudoJet(0.0,0.0,0.0,1e-300);
-//     }
-//   }
-//   jetmap[jet.E()] = jet.cluster_hist_index();
-// }
 
 
 FASTJET_END_NAMESPACE      // defined in fastjet/internal/base.hh
