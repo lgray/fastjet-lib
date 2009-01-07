b8e64266f2b5267990e6066693ef9aeb5df1d3fe
Gregory Soyez <gregory.soyez@cern.ch>
2009-01-07 05:20:09 +0000

    updated the 'make check' scripts and result files to account for the TrackJet output of many_algs_example
    git-svn-id: file:///Users/gsalam/svn/fastjet/trunk/fastjet-release@1346 9ab96302-e443-4389-8a14-f726967065a6

--- test-compare.sh
+++ test-compare.sh
@@ -115,11 +115,17 @@ else
     grep -v -e'^d0runiicone:' output_orig.tmp > output_orig_tmp.tmp
     mv output_orig_tmp.tmp output_orig.tmp
 fi
+if [[ -n `grep "define ENABLE_PLUGIN_TRACKJET" include/fastjet/config_auto.h` ]]; then
+    tested_plugins=${tested_plugins}"TrackJet "
+else
+    grep -v -e'^trackjet:' output_orig.tmp > output_orig_tmp.tmp
+    mv output_orig_tmp.tmp output_orig.tmp
+fi
 
 # 2. clear the orig output
 #     (i)  avoid line with '#' or 'SISCone' in them
 #     (ii) remove the plugins tags
-grep -v -e '#' -e 'SISCone' output_orig.tmp | sed -e "s/^pxcone://g" -e "s/^siscone://g" -e "s/^cdfmp://g" -e "s/^d0runiicone://g" > output_orig.tmp.tmp
+grep -v -e '#' -e 'SISCone' output_orig.tmp | sed -e "s/^pxcone://g" -e "s/^siscone://g" -e "s/^cdfmp://g" -e "s/^d0runiicone://g" -e "s/^trackjet://g" > output_orig.tmp.tmp
 mv output_orig.tmp.tmp output_orig.tmp
 
 # 2. clean the 'make check' output
