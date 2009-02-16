c6a7ca2cafb84bf488a5096a8489cf52b3125774
Gavin Salam <gavin.salam@cern.ch>
2009-02-16 23:35:41 +0000

    updated doc to reflect new 3.4 CGAL procedure
    git-svn-id: file:///Users/gsalam/svn/fastjet/trunk/fastjet-release@1461 9ab96302-e443-4389-8a14-f726967065a6

--- src/ClusterSequence.cc
+++ src/ClusterSequence.cc
@@ -106,8 +106,8 @@ void ClusterSequence::_initialise_and_run (
       // squared angular measure, make sure that the _R2 is defined
       // in a similar way.
       if (_Rparam > pi) {
-	// choose a value that ensures that back-toback particles will
-	//always recombine 
+	// choose a value that ensures that back-to-back particles will
+	// always recombine 
 	//_R2 = 4.0000000000001;
 	_R2 = 2 * ( 3.0 + cos(_Rparam) );
       } else {
