11f9ab4e901990529d14696c17b65de3e0c061e8
Gregory Soyez <gregory.soyez@cern.ch>
2009-05-28 09:43:26 +0000

    commented out ghost_area() in CSAAEG
    git-svn-id: file:///Users/gsalam/svn/fastjet/trunk/fastjet-release@1595 9ab96302-e443-4389-8a14-f726967065a6

--- include/fastjet/ClusterSequenceActiveAreaExplicitGhosts.hh
+++ include/fastjet/ClusterSequenceActiveAreaExplicitGhosts.hh
@@ -124,7 +124,7 @@ public:
   bool has_dangerous_particles() const {return _has_dangerous_particles;}
 
   /// get the area of the ghosts
-  double ghost_area() const{return _ghost_area;}
+  //double ghost_area() const{return _ghost_area;}
 
 private:
 
