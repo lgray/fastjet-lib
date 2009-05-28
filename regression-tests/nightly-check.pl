e3bb26a9a56530b7ec984ae8b3641ec10039a821
Gavin Salam <gavin.salam@cern.ch>
2009-05-28 09:23:48 +0000

    tiny change to nightly-check
    git-svn-id: file:///Users/gsalam/svn/fastjet/trunk/fastjet-release@1592 9ab96302-e443-4389-8a14-f726967065a6

--- regression-tests/nightly-check.pl
+++ regression-tests/nightly-check.pl
@@ -70,7 +70,7 @@ $mailAddr='salam@lpthe.jussieu.fr cacciari@lpthe.jussieu.fr gsoyez@quark.phy.bnl
 push @setups, ["karnak","--enable-allcxxplugins CC=/usr/local/bin/gcc-4.4 CXX=/usr/local/bin/g++-4.4", "", 10]; # full set with gcc 4.4 
 push @setups, ["","", "", 10]; # out of the box
 push @setups, ["","--enable-allcxxplugins --enable-cgal --with-cgaldir=".$ENV{CGAL_DIR}, "", 1000]; # with CGAL & all plugins
-# push @setups, ["","--enable-allcxxplugins --enable-shared --disable-static", "--runpath", 10]; # with dynlibs
+ push @setups, ["","--enable-allcxxplugins --enable-shared --disable-static", "--runpath", 10]; # with dynlibs
 # push @setups, ["","--enable-allcxxplugins --enable-shared", "--shared=no", 10]; # with static libs even though shared are built
 # push @setups, ["zetes", "", "", 10]; # out of the box on zetes (SLC4, gcc 3.4.6, 64 bit)
 # push @setups, ["karnak","", "", 10]; # out of the box on karnak (OS X 10.5)
