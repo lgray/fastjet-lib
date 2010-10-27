fe524b7297a3472afdcc76c5c6ff83552ec18cc7
Gregory Soyez <gregory.soyez@cern.ch>
2010-10-27 12:11:38 +0000

    updated the static build system (mostly for the plugins)
    git-svn-id: file:///Users/gsalam/svn/fastjet/trunk/fastjet-release@1799 9ab96302-e443-4389-8a14-f726967065a6

--- src/genconfig.sh
+++ src/genconfig.sh
@@ -18,8 +18,29 @@ echo '#define PACKAGE_VERSION  "'$packver'"'  >> $configfile
 # by default some plugins are define/disabled
 cat >> $configfile <<EOF
 
+/* The ATLASCone plugin is disabled by default*/
+#undef ENABLE_PLUGIN_ATLASCONE 
+
 /* The CDFJetClu and CDFMidPoint plugins are enabled by default*/
 #define ENABLE_PLUGIN_CDFCONES 
+
+/* The CMSIterativeCone plugin is disabled by default*/
+#undef ENABLE_PLUGIN_CMSITERATIVECONE 
+
+/* The D0RunICone plugin is disabled by default*/
+#undef ENABLE_PLUGIN_D0RUNICONE 
+
+/* The D0RunIICone plugin is disabled by default*/
+#undef ENABLE_PLUGIN_D0RUNIICONE 
+
+/* The EECambridge plugin is enabled by default*/
+#define ENABLE_PLUGIN_EECAMBRIDGE 
+
+/* The Jade plugin is enabled by default*/
+#define ENABLE_PLUGIN_JADE 
+
+/* The NestedDefs plugin is enabled by default*/
+#define ENABLE_PLUGIN_NESTEDDEFS 
 
 /* The PxCone plugin is disabled by default*/
 #undef ENABLE_PLUGIN_PXCONE
@@ -27,6 +48,8 @@ cat >> $configfile <<EOF
 /* The SISCone plugin is enabled by default*/
 #define ENABLE_PLUGIN_SISCONE 
 
+/* The TrackJet plugin is disabled by default*/
+#undef ENABLE_PLUGIN_TRACKJET
+
 /* end of plugin section */
 EOF
-
