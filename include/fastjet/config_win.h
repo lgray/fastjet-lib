477f306222f7f88ab48b566b21744982f3d8fc66
Gregory Soyez <gregory.soyez@cern.ch>
2011-01-27 16:08:12 +0000

    started the release process (mostly version numbers)
    git-svn-id: file:///Users/gsalam/svn/fastjet/trunk/fastjet-release@1894 9ab96302-e443-4389-8a14-f726967065a6

--- include/fastjet/config_win.h
+++ include/fastjet/config_win.h
@@ -1,11 +1,37 @@
-#define PACKAGE_STRING  "FastJet 2.5-devel"
-#define PACKAGE_VERSION  "2.5-devel"
+#define PACKAGE_STRING  "FastJet 3.0alpha1"
+#define PACKAGE_VERSION  "3.0alpha1"
+
+/* The ATLASCone plugin is disabled by default*/
+#undef ENABLE_PLUGIN_ATLASCONE 
 
 /* The CDFJetClu and CDFMidPoint plugins are enabled by default*/
 #define ENABLE_PLUGIN_CDFCONES 
+
+/* The CMSIterativeCone plugin is disabled by default*/
+#undef ENABLE_PLUGIN_CMSITERATIVECONE 
+
+/* The D0RunICone plugin is disabled by default*/
+#undef ENABLE_PLUGIN_D0RUNICONE 
+
+/* The D0RunIICone plugin is disabled by default*/
+#undef ENABLE_PLUGIN_D0RUNIICONE 
+
+/* The EECambridge plugin is enabled by default*/
+#define ENABLE_PLUGIN_EECAMBRIDGE 
+
+/* The Jade plugin is enabled by default*/
+#define ENABLE_PLUGIN_JADE 
+
+/* The NestedDefs plugin is enabled by default*/
+#define ENABLE_PLUGIN_NESTEDDEFS 
 
 /* The PxCone plugin is disabled by default*/
 #undef ENABLE_PLUGIN_PXCONE
 
 /* The SISCone plugin is enabled by default*/
 #define ENABLE_PLUGIN_SISCONE 
+
+/* The TrackJet plugin is disabled by default*/
+#undef ENABLE_PLUGIN_TRACKJET
+
+/* end of plugin section */
