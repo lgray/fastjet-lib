0d25ca69506dceadaf1be51ea49fde4b63082518
Gregory Soyez <gregory.soyez@cern.ch>
2011-01-28 18:14:53 +0000

    removed the non_const_cs() method to avoid a constness issue
    git-svn-id: file:///Users/gsalam/svn/fastjet/trunk/fastjet-release@1910 9ab96302-e443-4389-8a14-f726967065a6

--- include/fastjet/ClusterSequenceWrapper.hh
+++ include/fastjet/ClusterSequenceWrapper.hh
@@ -53,7 +53,6 @@ public:
   ClusterSequenceWrapper(ClusterSequence *cs) : _cs(cs){};
 
   const ClusterSequence * cs() const { return _cs;}
-  ClusterSequence * non_const_cs() const { return _cs;}
   bool is_alive() const { return (_cs != NULL);}
 
   friend class ClusterSequence;
