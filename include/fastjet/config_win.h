c8665b1ba76996e1c01010eebec7e2338c6e3f7b
Gavin Salam <gavin.salam@cern.ch>
2011-03-10 22:58:35 +0000

    finished off post-release part of CHECKLIST, including moving SISCone back to trunk and updating the version number fo 3.0a3-devel
    git-svn-id: file:///Users/gsalam/svn/fastjet/trunk/fastjet-release@2002 9ab96302-e443-4389-8a14-f726967065a6

--- include/fastjet/config_win.h
+++ include/fastjet/config_win.h
@@ -1,5 +1,5 @@
-#define FASTJET_PACKAGE_STRING  "FastJet 3.0alpha2"
-#define FASTJET_PACKAGE_VERSION  "3.0alpha2"
+#define FASTJET_PACKAGE_STRING  "FastJet 3.0alpha3-devel"
+#define FASTJET_PACKAGE_VERSION  "3.0alpha3-devel"
 
 /* The ATLASCone plugin is disabled by default*/
 #undef FASTJET_ENABLE_PLUGIN_ATLASCONE 
