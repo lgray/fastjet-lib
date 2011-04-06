1ced52c48d198f958bcbb3d18ed329d9533fe5e8
Gavin Salam <gavin.salam@cern.ch>
2011-04-06 15:03:59 +0000

    re-enabled karnak on nightly checks
    git-svn-id: file:///Users/gsalam/svn/fastjet/trunk/fastjet-release@2033 9ab96302-e443-4389-8a14-f726967065a6

--- regression-tests/nightly-check.pl
+++ regression-tests/nightly-check.pl
@@ -96,11 +96,12 @@ push @setups, ["tycho","--enable-allcxxplugins", "", 1000]; # tycho: standard ma
 #push @setups, ["karnak","FC=/usr/local/bin/gfortran-4.4", "", 10]; # out of the box on karnak (OS X 10.5)
 # karnak reenabled 2010-12-20
 # karnak disabled 2011-03-23
-# push @setups, ["karnak","", "", 10]; # out of the box on karnak (OS X 10.5)
-# push @setups, ["karnak","--enable-allcxxplugins", "", 1000]; # full monty on karnak
-# push @setups, ["karnak","--enable-allcxxplugins --disable-shared", "", 10]; # full monty on karnak
-# push @setups, ["karnak","--enable-allcxxplugins", "--shared=no", 10]; # full monty on karnak
-# push @setups, ["karnak","--enable-allcxxplugins CC=/usr/local/bin/gcc-4.4 CXX=/usr/local/bin/g++-4.4", "", 10]; # full set with gcc 4.4 
+# karnak enabled 2011-04-06
+push @setups, ["karnak","", "", 10]; # out of the box on karnak (OS X 10.5)
+push @setups, ["karnak","--enable-allcxxplugins", "", 1000]; # full monty on karnak
+push @setups, ["karnak","--enable-allcxxplugins --disable-shared", "", 10]; # full monty on karnak
+push @setups, ["karnak","--enable-allcxxplugins", "--shared=no", 10]; # full monty on karnak
+push @setups, ["karnak","--enable-allcxxplugins CC=/usr/local/bin/gcc-4.4 CXX=/usr/local/bin/g++-4.4", "", 10]; # full set with gcc 4.4 
 
 
 # process command-line
