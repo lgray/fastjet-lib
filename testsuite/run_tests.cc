482d1c5a79222133f05a13b05d99e8a035ba0987
Gavin Salam <gavin.salam@cern.ch>
2011-05-13 15:00:34 +0000

    allowed selection of a single test
    git-svn-id: file:///Users/gsalam/svn/fastjet/trunk/fastjet-release@2112 9ab96302-e443-4389-8a14-f726967065a6

--- testsuite/run_tests.cc
+++ testsuite/run_tests.cc
@@ -12,6 +12,7 @@ int main(int argc, char** argv) {
   CmdLine cmdline(argc,argv);
 
   bool verbose = cmdline.present("-verbose");
+  string only = cmdline.value<string>("-only", "");
 
   vector<TestBase *> tests;
 
@@ -22,6 +23,11 @@ int main(int argc, char** argv) {
   // loop over the tests
   for (unsigned i = 0; i < tests.size(); i++) {
     bool pass = tests[i]->run_test();
+
+    // allow the user to concentrate on one test series
+    if (only.size() != 0 && (only != tests[i]->short_name() &&
+			     "Test"+only != tests[i]->short_name())) continue;
+
     if (pass) {
       cout << setw(4) << i << "  PASS: " << tests[i]->short_name() <<endl;
       if (verbose) tests[i]->print_OK(cout, 15);
