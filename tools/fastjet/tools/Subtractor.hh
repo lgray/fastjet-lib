564353b51166e6649a2410b54e8380f66e774ce4
Gregory Soyez <gregory.soyez@cern.ch>
2011-05-17 06:30:43 +0000

    reinserted the use of a pointer (lost in an intermediate commit)
    git-svn-id: file:///Users/gsalam/svn/fastjet/trunk/fastjet-release@2139 9ab96302-e443-4389-8a14-f726967065a6

--- tools/fastjet/tools/Subtractor.hh
+++ tools/fastjet/tools/Subtractor.hh
@@ -61,7 +61,7 @@ FASTJET_BEGIN_NAMESPACE     // defined in fastjet/internal/base.hh
 class Subtractor : public Transformer{
 public:
   /// define a subtractor based on a BackgroundEstimator
-  Subtractor(BackgroundEstimator & bge) : 
+  Subtractor(BackgroundEstimator * bge) : 
     _bge(bge) {}
 
   /// default dtor
@@ -81,7 +81,7 @@ public:
 protected:
   /// the tool used to estimate the background
   /// if has to be mutable in case its underlying selector takes a reference jet
-  mutable BackgroundEstimator _bge;
+  mutable BackgroundEstimator * _bge;
 };
 
 FASTJET_END_NAMESPACE
