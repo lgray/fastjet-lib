a50128b8cb46e9b4bb45c83b217e1c84c4c2f7ad
Gregory Soyez <gregory.soyez@cern.ch>
2011-06-03 20:54:43 +0000

    final commit for fastjet 3.0alpha3
    git-svn-id: file:///Users/gsalam/svn/fastjet/trunk/fastjet-release@2242 9ab96302-e443-4389-8a14-f726967065a6

--- include/fastjet/config_win.h
+++ include/fastjet/config_win.h
@@ -1,5 +1,5 @@
-#define FASTJET_PACKAGE_STRING  "FastJet 3.0alpha3-devel"
-#define FASTJET_PACKAGE_VERSION  "3.0alpha3-devel"
+#define FASTJET_PACKAGE_STRING  "FastJet 3.0alpha3"
+#define FASTJET_PACKAGE_VERSION  "3.0alpha3"
 
 /* The ATLASCone plugin is disabled by default*/
 #undef FASTJET_ENABLE_PLUGIN_ATLASCONE 
