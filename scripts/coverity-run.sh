4661ea36adab9a0bb1c4ea027bb284487965cb7a
Gavin Salam <gavin.salam@cern.ch>
2016-03-03 10:28:29 +0000

    minor fix to coverity run script
    git-svn-id: file:///Users/gsalam/svn/fastjet/trunk/fastjet-release@4046 9ab96302-e443-4389-8a14-f726967065a6

--- scripts/coverity-run.sh
+++ scripts/coverity-run.sh
@@ -12,7 +12,7 @@ COV_BIN=/coverity/cov-analysis/bin/
 
 # build
 $COV_BIN/cov-build --dir cov-out make -j 24 || exit 4
-tail $PATHDIR/cov-out/build-log.txt || exit 5
+tail cov-out/build-log.txt || exit 5
 
 # run 
 $COV_BIN/cov-analyze --dir cov-out  --cpp -j auto --enable-callgraph-metrics --enable-parse-warnings || exit 6
