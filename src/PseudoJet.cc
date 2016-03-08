3e37cf9b059a9ddefdf9484be05d3cddfef2b47a
Gregory Soyez <gregory.soyez@cern.ch>
2016-03-08 08:22:04 +0000

    made an explicit conversion to bool for PJ::has_structure()
    git-svn-id: file:///Users/gsalam/svn/fastjet/trunk/fastjet-release@4073 9ab96302-e443-4389-8a14-f726967065a6

--- src/PseudoJet.cc
+++ src/PseudoJet.cc
@@ -468,7 +468,7 @@ void PseudoJet::set_structure_shared_ptr(const SharedPtr<PseudoJetStructureBase>
 //----------------------------------------------------------------------
 // return true if there is some strusture associated with this PseudoJet
 bool PseudoJet::has_structure() const{
-  return _structure;
+  return (bool) _structure;
 }
 
 //----------------------------------------------------------------------
