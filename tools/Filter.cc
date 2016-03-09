f9f6863de9c1acdf1729465c90323ccdbf9cf335
Gregory Soyez <gregory.soyez@cern.ch>
2016-03-09 15:01:57 +0000

    used Subtractor(rho) instead of the deprecated CSAB interface for Filters with an external rho
    git-svn-id: file:///Users/gsalam/svn/fastjet/trunk/fastjet-release@4080 9ab96302-e443-4389-8a14-f726967065a6

--- tools/Filter.cc
+++ tools/Filter.cc
@@ -30,6 +30,7 @@
 
 #include "fastjet/tools/Filter.hh"
 #include "fastjet/tools/Recluster.hh"
+#include "fastjet/tools/Subtractor.hh"
 #include <fastjet/ClusterSequenceActiveAreaExplicitGhosts.hh>
 #include <cassert>
 #include <algorithm>
@@ -97,9 +98,10 @@ PseudoJet Filter::result(const PseudoJet &jet) const {
     subjets = (*_subtractor)(subjets);
   } else if (_rho!=0){
     if (subjets.size()>0){
-      const ClusterSequenceAreaBase *csab = subjets[0].validated_csab();
+      //const ClusterSequenceAreaBase *csab = subjets[0].validated_csab();
       for (unsigned int i=0;i<subjets.size();i++){
-        subjets[i]=csab->subtracted_jet(subjets[i], _rho);
+        //subjets[i]=csab->subtracted_jet(subjets[i], _rho);
+        subjets[i]=Subtractor(_rho)(subjets[i]);
       }
     }
   }
