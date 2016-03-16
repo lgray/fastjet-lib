cb9981f3b94f54dc76ec2d42a5ffb5c7d6263015
Gregory Soyez <gregory.soyez@cern.ch>
2016-03-16 13:16:30 +0000

    switched version number to 3.2.0 and prepared for the release
    git-svn-id: file:///Users/gsalam/svn/fastjet/trunk/fastjet-release@4104 9ab96302-e443-4389-8a14-f726967065a6

--- include/fastjet/config_win.h
+++ include/fastjet/config_win.h
@@ -1,10 +1,9 @@
-#define FASTJET_PACKAGE_STRING  "FastJet 3.2.0-devel"
-#define FASTJET_PACKAGE_VERSION  "3.2.0-devel"
-#define FASTJET_VERSION  "3.2.0-devel"
+#define FASTJET_PACKAGE_STRING  "FastJet 3.2.0"
+#define FASTJET_PACKAGE_VERSION  "3.2.0"
+#define FASTJET_VERSION  "3.2.0"
 #define FASTJET_VERSION_MAJOR       3
 #define FASTJET_VERSION_MINOR       2
 #define FASTJET_VERSION_PATCHLEVEL  0
-#define FASTJET_VERSION_PRERELEASE  "devel"
 #define FASTJET_VERSION_NUMBER      30200
 
 /* The ATLASCone plugin is disabled by default*/
