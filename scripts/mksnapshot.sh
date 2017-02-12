d8f9153d7b7f0dd20505e238ec9d4490a08cae4d
Gavin Salam <gavin.salam@cern.ch>
2017-02-12 17:26:01 +0000

    tried to fix links in mksnapshop.sh for use remotely
    git-svn-id: file:///Users/gsalam/svn/fastjet/trunk/fastjet-release@4256 9ab96302-e443-4389-8a14-f726967065a6

--- scripts/mksnapshot.sh
+++ scripts/mksnapshot.sh
@@ -36,7 +36,7 @@ echo "Have produced the file(s):"
 filename=`ls -rt *$extralabel*.tar.gz | tail -1`
 filebase=`echo $filename | sed 's/.tar.gz//'`
 echo Now run 
-echo mv -v $filename '~'salam/www/fastjet/repository/snapshots/
-echo svn cp . file:///ada1/lpthe/salam/svn/fastjet/tags/snapshots/$filebase -m \'tagged $filebase snapshot\'
-echo \# edit '~'salam/www/fastjet3/snapshots.html
+echo scp -p $filename tycho.lpthe.jussieu.fr:'~'salam/www/fastjet/repository/snapshots/
+echo svn cp . svn+ssh://tycho.lpthe.jussieu.fr/ada1/lpthe/salam/svn/fastjet/tags/snapshots/$filebase -m \'tagged $filebase snapshot\'
+echo \# edit '~'salam/www/fastjet3/snapshots.html on tycho
 #mv -v *$extralabel*.tar.gz ~salam/www/fastjet/repository/snapshots/
