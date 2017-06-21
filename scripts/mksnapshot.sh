832be1cc3f977d13b96a382b6e29600f64698536
Gavin Salam <gavin.salam@cern.ch>
2017-06-21 10:03:35 +0000

    mksnapshot.sh now also helps with generation of the updated html file
    git-svn-id: file:///Users/gsalam/svn/fastjet/trunk/fastjet-release@4309 9ab96302-e443-4389-8a14-f726967065a6

--- scripts/mksnapshot.sh
+++ scripts/mksnapshot.sh
@@ -9,34 +9,73 @@ fi
 # get an up to date version
 svn update || exit -1
 
-# get the revision number
+# get the revision number 
 rev=`svn info | grep Revision | sed s'/[^0-9]*//'`
-
-# extra label will be
+# and then generate an extra label for the version number, including also the date
 extralabel=`date +"%Y%m%d"`-rev$rev
-echo $extralabel
+echo "Extra label is: $extralabel"
+
+#----------------------------------------------------------------------
+# Create a new tarball (and then put things back to normal)
+#
+# update version numbers in some critical files (why not use ./scripts/set-version ???)
 sed 's/\(AC_INIT.*\)])/\1-'$extralabel'])/' < configure.ac > configure.ac.new
 mv configure.ac.new configure.ac
-
-# now make sure the windows config file is consistent
 pushd src
 ./genconfig.sh ../include/fastjet/config_win.h
 popd
 
 # now make and test the distribution 
-make -j2 distcheck 
+make -j4 distcheck 
 
 # and put the configure file back to where it was
 svn revert configure.ac
 svn revert include/fastjet/config_win.h
 
-# and tell the user you have a result
-echo "**************************************************"
-echo "Have produced the file(s):"
+# then get things ready for the output for manual operations
 filename=`ls -rt *$extralabel*.tar.gz | tail -1`
 filebase=`echo $filename | sed 's/.tar.gz//'`
+
+#----------------------------------------------------------------------
+# create a patch file corresponding to the edits needed for the snapshot file
+remoteSnapshotHTML='tycho.lpthe.jussieu.fr:~salam/www/fastjet3/snapshots.html'
+oldSnapshotHTML='/tmp/snapshots-incoming.html'
+newSnapshotHTML='/tmp/snapshots-'$extralabel.html
+diffSnapshotHTML='/tmp/snapshots-diff-'$extralabel.html
+scp -p $remoteSnapshotHTML $oldSnapshotHTML
+if [[ -e $newSnapshotHTML ]]; then
+    rm $newSnapshotHTML
+fi
+touch $newSnapshotHTML
+addedLine=false
+while IFS='' read -r line || [[ -n "$line" ]]; do
+    echo "$line" >> $newSnapshotHTML
+    if [[ $line =~ 'ul id=snapshots' ]]; then
+        echo '    <li> <a href="repository/snapshots/'"$filename"'">'"$filename"'</a>' >> $newSnapshotHTML
+        addedLine=true
+    fi
+done < "$oldSnapshotHTML"
+# if we were successful then 
+if [[ $addedLine ]]; then
+    diff -u $oldSnapshotHTML $newSnapshotHTML > $diffSnapshotHTML
+    echo "Snapshot file diff created as $diffSnapshotHTML"
+    cat $diffSnapshotHTML
+else
+    echo "Failed to correctly create $newSnapshoHTML"
+fi
+
+#----------------------------------------------------------------------
+# and tell the user you have a result and what they should do
+echo "**************************************************"
+echo "Have produced the file: $filename"
 echo Now run 
 echo scp -p $filename tycho.lpthe.jussieu.fr:'~'salam/www/fastjet/repository/snapshots/
 echo svn cp svn+ssh://tycho.lpthe.jussieu.fr/ada1/lpthe/salam/svn/fastjet/trunk/fastjet-release svn+ssh://tycho.lpthe.jussieu.fr/ada1/lpthe/salam/svn/fastjet/tags/snapshots/$filebase -m \'tagged $filebase snapshot\'
-echo \# edit '~'salam/www/fastjet3/snapshots.html on tycho
-#mv -v *$extralabel*.tar.gz ~salam/www/fastjet/repository/snapshots/
+echo
+
+# watch out: we apply a patch so as not to modify special permissions on the file
+# (e.g. fastjet group write access, +x flag, etc.)
+if [[ $addedLine ]]; then
+    echo "scp -p $diffSnapshotHTML tycho.lpthe.jussieu.fr:$diffSnapshotHTML"
+    echo "ssh tycho.lpthe.jussieu.fr patch /ada1/lpthe/salam/www/fastjet3/snapshots.html $diffSnapshotHTML"
+fi
