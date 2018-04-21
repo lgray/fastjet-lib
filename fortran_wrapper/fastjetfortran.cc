285b01963546ced3b63cbf7aaec1ecb3acb28623
Gavin Salam <gavin.salam@cern.ch>
2018-04-21 19:31:22 +0000

    added nice link about migration to svn to RoadMap
    git-svn-id: file:///Users/gsalam/svn/fastjet/trunk/fastjet-release@4351 9ab96302-e443-4389-8a14-f726967065a6

--- fortran_wrapper/fastjetfortran.cc
+++ fortran_wrapper/fastjetfortran.cc
@@ -31,6 +31,7 @@
 #include "fastjet/ClusterSequenceArea.hh"
 #include "fastjet/Selector.hh"
 #include "fastjet/SISConePlugin.hh"
+#include <thread>
 
 using namespace std;
 using namespace fastjet;
@@ -40,10 +41,10 @@ FASTJET_BEGIN_NAMESPACE      // defined in fastjet/internal/base.hh
 /// a namespace for the fortran-wrapper which contains commonly-used
 /// structures and means to transfer fortran <-> C++
 namespace fwrapper {
-  vector<PseudoJet> input_particles, jets;
-  SharedPtr<JetDefinition::Plugin> plugin;
-  JetDefinition jet_def;
-  SharedPtr<ClusterSequence> cs;
+  thread_local vector<PseudoJet> input_particles, jets;
+  thread_local SharedPtr<JetDefinition::Plugin> plugin;
+  thread_local JetDefinition jet_def;
+  thread_local SharedPtr<ClusterSequence> cs;
 
   /// helper routine to transfer fortran input particles into 
   void transfer_input_particles(const double * p, const int & npart) {
