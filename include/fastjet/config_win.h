ca373624774142b8a614a995256779dd5a0bc360
Gregory Soyez <gregory.soyez@cern.ch>
2019-11-29 13:15:47 +0000

    started the work towards the release of FastJet 3.3.3 (reached the nightly-check stage)
    git-svn-id: file:///Users/gsalam/svn/fastjet/trunk/fastjet-release@4422 9ab96302-e443-4389-8a14-f726967065a6

--- include/fastjet/config_win.h
+++ include/fastjet/config_win.h
@@ -1,10 +1,9 @@
-#define FASTJET_PACKAGE_STRING  "FastJet 3.3.3-devel"
-#define FASTJET_PACKAGE_VERSION  "3.3.3-devel"
-#define FASTJET_VERSION  "3.3.3-devel"
+#define FASTJET_PACKAGE_STRING  "FastJet 3.3.3"
+#define FASTJET_PACKAGE_VERSION  "3.3.3"
+#define FASTJET_VERSION  "3.3.3"
 #define FASTJET_VERSION_MAJOR       3
 #define FASTJET_VERSION_MINOR       3
 #define FASTJET_VERSION_PATCHLEVEL  3
-#define FASTJET_VERSION_PRERELEASE  "devel"
 #define FASTJET_VERSION_NUMBER      30303
 
 /* The ATLASCone plugin is disabled by default*/
