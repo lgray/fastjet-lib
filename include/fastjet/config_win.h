ac3c3468914056548434f65ced876d395efcc541
Gregory Soyez <gregory.soyez@cern.ch>
2021-06-25 15:38:16 +0200

    switched version number to 3.4.0

--- include/fastjet/config_win.h
+++ include/fastjet/config_win.h
@@ -1,10 +1,9 @@
-#define FASTJET_PACKAGE_STRING  "FastJet 3.4.0-devel"
-#define FASTJET_PACKAGE_VERSION  "3.4.0-devel"
-#define FASTJET_VERSION  "3.4.0-devel"
+#define FASTJET_PACKAGE_STRING  "FastJet 3.4.0"
+#define FASTJET_PACKAGE_VERSION  "3.4.0"
+#define FASTJET_VERSION  "3.4.0"
 #define FASTJET_VERSION_MAJOR       3
 #define FASTJET_VERSION_MINOR       4
 #define FASTJET_VERSION_PATCHLEVEL  0
-#define FASTJET_VERSION_PRERELEASE  "devel"
 #define FASTJET_VERSION_NUMBER      30400
 
 /* The ATLASCone plugin is disabled by default*/
